4e1c6f57c58a8535ac5303309597e86eb4c32dda
Matt <matt.crocker@gmail.com>
2023-05-31 12:11:23 +0100

    Update schema name

--- config.py
+++ config.py
@@ -13,6 +13,9 @@ class Config:
     HOST = os.getenv("HOST", "0.0.0.0")
     PORT = os.getenv("PORT", 5051)
 
+    DATABASE_SCHEMA = os.getenv("DATABASE_SCHEMA", "securemessage")
+    DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres")
+
 
 class TestConfig(Config):
     """
