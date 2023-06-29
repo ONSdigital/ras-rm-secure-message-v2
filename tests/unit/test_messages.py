b043ae908b8002833aed97e1d8998eef2720785c
Matt <matt.crocker@gmail.com>
2023-06-29 16:51:49 +0100

    Adding almost certainly failing tests

--- tests/unit/test_messages.py
+++ tests/unit/test_messages.py
@@ -1,9 +1,22 @@
-import unittest
+import json
+from unittest import TestCase
+
+from mock import patch
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 
 from secure_message_v2.application import create_app
+from secure_message_v2.controllers.messages import post_new_message
+
+good_payload = {
+    "thread_id": "1f2324b9-b0ee-4fad-91c5-3539fd42fef7",
+    "body": "Hi this is a message",
+    "is_from_internal": True,
+    "sent_by": "26410f78-1731-421f-a191-128833a1055c",
+}
+bad_payload = {"body": "Hi this is a message", "is_from_internal": True}
 
 
-class TestMessages(unittest.TestCase):
+class TestMessages(TestCase):
     def setUp(self):
         self.app = create_app(config="TestConfig")
         self.client = self.app.test_client()
@@ -11,7 +24,23 @@ class TestMessages(unittest.TestCase):
 
     def test_post_message_bad_payload_returns_400(self):
         # Payload is missing thread_id and sent_by
-        bad_payload = {"body": "Hi this is a message", "is_from_internal": True}
         with self.app.app_context():
             response = self.client.post("/messages", json=bad_payload, follow_redirects=True)
             self.assertEqual(400, response.status_code)
+
+    @patch("controllers.messages.post_new_message", return_value={"id": "abcdef"})
+    def test_successful_post_message_returns_201(self, mock):
+        with self.app.app_context():
+            response = self.client.post("/messages", json=good_payload, follow_redirects=True)
+            response_data = json.loads(response.get_data())
+            expected_result = json.dumps({"id": "abcdef"})
+            self.assertEqual(201, response.status_code)
+            self.assertEqual(expected_result, response_data)
+
+    def test_message_created_when_called(self):
+        session = UnifiedAlchemyMagicMock()
+        result = post_new_message(good_payload, session)
+        session.add.assert_any_call()
+        self.assertDictContainsSubset(result, good_payload)
+        self.assertTrue("id" in result)
+        self.assertTrue("sent_at" in result)
