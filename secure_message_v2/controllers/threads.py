9b4465d34883880224e4620293cbd2a6b1d0f619
Matt <matt.crocker@gmail.com>
2023-07-04 15:38:51 +0100

    Bad syntax

--- secure_message_v2/controllers/threads.py
+++ secure_message_v2/controllers/threads.py
@@ -20,16 +20,16 @@ def post_new_thread(message, session):
     thread = Thread(
         subject=message["subject"],
         category=message["category"],
-        is_closed=message["is_closed"] or False,
-        closed_by_id=message["closed_by_id"] or None,
-        closed_at=message["closed_at"] or None,
-        case_id=message["case_id"] or None,
-        ru_ref=message["ru_ref"] or None,
-        survey_id=message["survey_id"] or None,
-        assigned_internal_user_id=message["assigned_internal_user_id"] or None,
-        respondent_id=message["respondent_id"] or None,
-        is_read_by_respondent=message["is_read_by_respondent"] or False,
-        is_read_by_internal=message["is_read_by_internal"] or False,
+        is_closed=message.get("is_closed", False),
+        closed_by_id=message.get("closed_by_id", None),
+        closed_at=message.get("closed_at", None),
+        case_id=message.get("case_id", None),
+        ru_ref=message.get("ru_ref", None),
+        survey_id=message.get("survey_id", None),
+        assigned_internal_user_id=message.get("assigned_internal_user_id", None),
+        respondent_id=message.get("respondent_id", None),
+        is_read_by_respondent=message.get("is_read_by_respondent", False),
+        is_read_by_internal=message.get("is_read_by_internal", False),
         # We default to unread for both of these, as posting the message after will set the flags properly
     )
 
